93c2eb004a0e75aa6460d63034011ed5b2579a69
Luca Pizzini <lpizzini7@gmail.com>
2021-12-13 16:12:45 +0100

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nicola Dalle Vedove on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
